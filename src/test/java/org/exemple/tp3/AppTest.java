e46384adae75e14509c24af96a955dc71554c769
ZinebAROHANE <arohane.zineb@gmail.com>
2020-04-02 00:01:14 +0000

    First commit maven project

---
+++ src/test/java/org/exemple/tp3/AppTest.java
@@ -0,0 +1,12 @@
+package org.exemple.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
